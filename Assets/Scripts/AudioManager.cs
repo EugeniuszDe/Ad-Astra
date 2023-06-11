47248c9055d09c92402d7de758542e6aaeeb1b6d
EugeniuszDe <123085362+EugeniuszDe@users.noreply.github.com>
2023-06-11 20:17:58 +0200

    AudioManager.cs

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,19 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// The audio manager
-/// </summary>
 public static class AudioManager
 {
     static AudioSource audioSource;
     static Dictionary<AudioClipName, AudioClip> audioClips =
         new Dictionary<AudioClipName, AudioClip>();
 
-    /// <summary>
-    /// Initializes the audio manager
-    /// </summary>
-    /// <param name="source">audio source</param>
+    // Initializes the audio manager
     public static void Initialize(AudioSource source)
     {
         audioSource = source;
@@ -26,10 +20,8 @@ public static class AudioManager
             Resources.Load<AudioClip>("shoot"));
     }
 
-    /// <summary>
-    /// Plays the audio clip with the given name
-    /// </summary>
-    /// <param name="name">name of the audio clip to play</param>
+    // Plays the audio clip with the given name
+    // <param name="name">name of the audio clip to play</param>
     public static void Play(AudioClipName name)
     {
         audioSource.PlayOneShot(audioClips[name]);
