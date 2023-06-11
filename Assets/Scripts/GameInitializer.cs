aed7554e81345f28deacd5c9b728ddc682224d9b
EugeniuszDe <123085362+EugeniuszDe@users.noreply.github.com>
2023-06-11 20:20:38 +0200

    GameInitializer.cs

--- Assets/Scripts/GameInitializer.cs
+++ Assets/Scripts/GameInitializer.cs
@@ -2,14 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Initializes the game
-/// </summary>
 public class GameInitializer : MonoBehaviour 
 {
-    /// <summary>
-    /// Awake is called before Start
-    /// </summary>
 	void Awake()
     {
         // initialize screen utils
